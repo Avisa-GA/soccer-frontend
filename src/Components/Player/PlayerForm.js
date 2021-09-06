5f47b2919362a068dca8238a3210f5f2698f9819
Avisa-SEI <aap571420@gmail.com>
2021-09-06 16:08:10 -0700

    wip: Add the form base structure

--- src/Components/Player/PlayerForm.js
+++ src/Components/Player/PlayerForm.js
@@ -6,7 +6,40 @@ class PlayerForm extends React.Component {
     this.state = {};
   }
   render() {
-    return <div>Player Form</div>;
+    return (
+      <div className="row">
+        <h1 className="center">Add a new player</h1>
+        <form className="col s12" onSubmit={this.submitPlayer.bind(this)}>
+          <div className="row">
+            <div className="input-field col s6">
+              <input id="firstName" ref="firstName" type="text" />
+              <label htmlfor="firstName">First Name</label>
+            </div>
+            <div className="input-field col s6">
+              <input id="lastName" ref="lastName" type="text" />
+              <label htmlfor="lastName">Last Name</label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="input-field col s6">
+              <input id="phone" ref="phone" type="text" />
+              <label htmlfor="phone">Phone</label>
+            </div>
+            <div className="input-field col s6">
+              <input id="email" ref="email" type="text" />
+              <label htmlfor="email">Email</label>
+            </div>
+          </div>
+          <button
+            className="btn wave-effect wave-light"
+            type="submit"
+            name="action"
+          >
+            Add Player
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
