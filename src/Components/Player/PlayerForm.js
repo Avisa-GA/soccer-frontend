665f4fd1a9f249ba03f5144403c204c292aa1c03
Avisa-SEI <aap571420@gmail.com>
2021-09-06 16:22:28 -0700

    wip: finalize the form

--- src/Components/Player/PlayerForm.js
+++ src/Components/Player/PlayerForm.js
@@ -1,33 +1,45 @@
 import React from "react";
-
+import axios from "axios";
 class PlayerForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
+  submitPlayer(event) {
+    event.preventDefault();
+    axios
+      .post("http://localhost:4000/players", {
+        firstName: this.refs.firstName.value,
+        lastName: this.refs.lastName.value,
+        phone: this.refs.phone.value,
+        email: this.refs.email.value,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
   render() {
     return (
-      <div className="row">
+      <div className="row container">
         <h1 className="center">Add a new player</h1>
         <form className="col s12" onSubmit={this.submitPlayer.bind(this)}>
           <div className="row">
             <div className="input-field col s6">
               <input id="firstName" ref="firstName" type="text" />
-              <label htmlfor="firstName">First Name</label>
+              <label htmlFor="firstName">First Name</label>
             </div>
             <div className="input-field col s6">
               <input id="lastName" ref="lastName" type="text" />
-              <label htmlfor="lastName">Last Name</label>
+              <label htmlFor="lastName">Last Name</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s6">
               <input id="phone" ref="phone" type="text" />
-              <label htmlfor="phone">Phone</label>
+              <label htmlFor="phone">Phone</label>
             </div>
             <div className="input-field col s6">
               <input id="email" ref="email" type="text" />
-              <label htmlfor="email">Email</label>
+              <label htmlFor="email">Email</label>
             </div>
           </div>
           <button
